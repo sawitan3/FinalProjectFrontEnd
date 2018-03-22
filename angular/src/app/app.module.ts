ea4016994b99c089c1587d535a67339a151d270a
Sarah Sukamto <sarah.sukamto1998@gmail.com>
2018-03-22 22:45:49 +0700

    Created Services and Components
    Services:
    - RestApi
    - CRUD
    - Auth
    
    Components:
    - Navbar
    - Footer
    - Register
    - Login

--- angular/src/app/app.module.ts
+++ angular/src/app/app.module.ts
@@ -4,11 +4,19 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
+import { RegisterComponent } from './components/register/register.component';
+import { LoginComponent } from './components/login/login.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RegisterComponent,
+    LoginComponent,
+    NavbarComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
