3406eb6083343c1eb0f2bc62d0c7bf3c3d1ce01b
hansvin <hantansu98@gmail.com>
2018-03-26 10:33:45 +0700

    lupa commit

--- angular/src/app/services/crud.service.ts
+++ angular/src/app/services/crud.service.ts
@@ -1,8 +1,39 @@
 import { Injectable } from '@angular/core';
+import {HttpHeaders} from '@angular/common/http';
+import {Router} from '@angular/router';
+import {RestService} from './rest.service';
+import {ItemService} from './item.service';
 
 @Injectable()
 export class CrudService {
 
-  constructor() { }
+  constructor(private rest: RestService, private router: Router, private data: ItemService) { }
+  header = {headers: new HttpHeaders({
+      'Content-Type': 'application/json',
+      'Authorization': 'Bearer ' + localStorage.getItem('token')
+  })};
 
+  load(route, next){
+    this.rest.get(route, this.header).subscribe(res =>{
+      this.data = res;
+    });
+  }
+
+  create(route, body){
+    this.rest.post(route, body, this.header).subscribe(res =>{
+      this.data = res;
+    });
+  }
+
+  update(route, body){
+    this.rest.put(route, body, this.header).subscribe(res =>{
+      this.data = res;
+    });
+  }
+
+  delete(route){
+    this.rest.delete(route, this.header).subscribe(res =>{
+      this.data = res;
+    });
+  }
 }
