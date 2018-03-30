fc3d185dc7f9f8472747534a598739a88cf0b30e
〔≡◉︵◉≡〕 <anakaiti@users.noreply.github.com>
2018-03-30 21:19:42 +0700

    Install bootstrap locally

--- angular/src/app/app.module.ts
+++ angular/src/app/app.module.ts
@@ -6,7 +6,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { RestService } from './services/rest.service';
 import { CrudService } from './services/crud.service';
 import { AuthService } from './services/auth.service';
-import {ItemService} from "./services/item.service";
+import {ItemService} from './services/item.service';
 
 import { AppComponent } from './app.component';
 import { RegisterComponent } from './components/register/register.component';
